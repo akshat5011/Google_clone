2272d7d48d3a43694e3fc569f26ae713b7fe8679
akshat5011 <akshatgarg2k4@gmail.com>
2025-02-02 21:49:20 +0530

    fetch using google API Programmable Search Engine

--- src/components/SearchHeaderOptions.jsx
+++ src/components/SearchHeaderOptions.jsx
@@ -18,16 +18,16 @@ export default function SearchHeaderOptions() {
     <div className="flex space-x-2 select-none border-b w-full justify-center lg:justify-start text-gray-500 lg:pl-52">
       <div
         onClick={() => selectTab("All")}
-        className={`flex items-center border-b-4 border-transparent active:text-black cursor-pointer pb-3 px-2 ${
-          pathname == "/search/web" && "border-gray-700 text-gray-700"
+        className={`flex items-center text-sm border-b-4 border-transparent active:text-black cursor-pointer pb-3 px-2 ${
+          pathname == "/search/web" && "!border-gray-700 !text-gray-700"
         } `}
       >
         All
       </div>
       <div
         onClick={() => selectTab("Image")}
-        className={`flex items-center border-b-4 border-transparent active:text-black cursor-pointer pb-3 px-2 ${
-          pathname == "/search/images" && "border-gray-700 text-gray-700"
+        className={`flex items-center text-sm border-b-4 border-transparent active:text-black cursor-pointer pb-3 px-2 ${
+          pathname == "/search/images" && "!border-gray-700 !text-gray-700"
         } `}
       >
         Images
