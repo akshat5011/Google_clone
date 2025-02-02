ca0c6684231457c0feac0507c626232598845938
akshat5011 <akshatgarg2k4@gmail.com>
2025-02-03 00:18:25 +0530

    ImageSearchResult

--- src/app/search/images/page.jsx
+++ src/app/search/images/page.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import ImageSearchResult from "@/components/ImageSearchResult";
+import Link from "next/link";
+import React from "react";
 
-export default function ImageSearchPage () {
-  return (
-    <div> ImageSearchPage </div>
-  )
+export default async function ImageSearchPage({ searchParams }) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const searchpara = await searchParams;
+  const searchTerm = searchpara.searchTerm;
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchTerm}&searchType=image`
+  );
+  if (!response.ok) {
+    throw new Error("Something went wrong");
+  }
+
+  const data = await response.json();
+  const results = data.items;
+
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4"> No Results Found...</h1>
+        <p className="text-lg">Go Back</p>
+        <Link href={"/"} className="text-blue-500">
+          HOME
+        </Link>
+      </div>
+    );
+  }
+  return <>{results && <ImageSearchResult results={data} />}</>;
 }
