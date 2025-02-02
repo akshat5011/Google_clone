9338d5209df94b7b5daaddef6ee302ba80faecda
akshat5011 <akshatgarg2k4@gmail.com>
2025-02-02 21:10:12 +0530

    complete SearchHeaderOptions

--- src/components/SearchHeaderOptions.jsx
+++ src/components/SearchHeaderOptions.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React from "react";
 
 export default function SearchHeaderOptions() {
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParam = useSearchParams();
+  function selectTab(tab) {
+    router.push(
+      `/search/${tab === "All" ? "web" : "images"}?searchTerm=${searchParam.get(
+        "searchTerm"
+      )}`
+    );
+  }
   return (
-    <div>SearchHeaderOptions</div>
-  )
+    <div className="flex space-x-2 select-none border-b w-full justify-center lg:justify-start text-gray-500 lg:pl-52">
+      <div
+        onClick={() => selectTab("All")}
+        className={`flex items-center border-b-4 border-transparent active:text-black cursor-pointer pb-3 px-2 ${
+          pathname == "/search/web" && "border-gray-700 text-gray-700"
+        } `}
+      >
+        All
+      </div>
+      <div
+        onClick={() => selectTab("Image")}
+        className={`flex items-center border-b-4 border-transparent active:text-black cursor-pointer pb-3 px-2 ${
+          pathname == "/search/images" && "border-gray-700 text-gray-700"
+        } `}
+      >
+        Images
+      </div>
+    </div>
+  );
 }
