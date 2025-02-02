c29fcd02410740e3c6321da5537ce7958b67ce1b
akshat5011 <akshatgarg2k4@gmail.com>
2025-02-02 22:08:22 +0530

    error handling

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,15 +1,27 @@
+import Link from "next/link";
 import React from "react";
 
 export default async function WebSearchPage({ searchParams }) {
   const response = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${
-      process.env.CONTEXT_KEY
-    }&q=${searchParams.searchTerm}`
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
+  if (!response.ok) {
+    throw new Error("Something went wrong");
+  }
+
   const data = await response.json();
   const results = data.items;
 
-  return (
-    <>{results && results.map((result) => <h1>{result.title}</h1>)}</>
-  );
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4"> No Results Found...</h1>
+        <p className="text-lg">Go Back</p>
+        <Link href={"/"} className="text-blue-500">
+          HOME
+        </Link>
+      </div>
+    );
+  }
+  return <>{results && results.map((result) => <h1>{result.title}</h1>)}</>;
 }
